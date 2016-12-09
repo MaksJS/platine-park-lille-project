e5edf1aa12a878c14b4263701cdb15ce7920781b
Maks <maxence.derous@gmail.com>
2016-12-09 17:40:28 +0100

    added post route to insert space and get route to get last state of a space

--- platine/routes/web.php
+++ platine/routes/web.php
@@ -16,27 +16,52 @@ Route::get('/', function () {
 });
 
 Route::get('/spaces', function (Request $request) {
-    return response()->json(json_decode(file_get_contents('data.json'), true));
+    return response()->json(json_decode(file_get_contents('data.json'), true), 200);
 });
 
 Route::get('/spaces/{building}', function (Request $request, $building) {
     $data = json_decode(file_get_contents('data.json'), true);
     if (array_key_exists($building, $data))
-        return response()->json($data[$building]);
+        return response()->json($data[$building], 200);
     else abort(404);
 });
 
-Route::get('/space/{building}/{id}', function (Request $request, $building, $id) {
+Route::get('/space/{building}/{space}', function (Request $request, $building, $space) {
     $data = json_decode(file_get_contents('data.json'), true);    
-    if (array_key_exists($building, $data) && array_key_exists($id, $data[$building]))  
-        return response()->json($data[$building][$id]);
+    if (array_key_exists($building, $data) && array_key_exists($space, $data[$building]))  
+        return response()->json($data[$building][$space], 200);
     else abort(404);
 });
 
-Route::post('/space/{building}/{id}/occuped', function (Request $request) {
-    
+Route::post('/space/{building}/{space}/occuped', function (Request $request, $building, $space) {
+    \DB::table('spaces')->insert(
+        [
+            'building' => $building, 
+            'space' => $space,
+            'free' => false,
+        ]
+    );
+    return response()->json(true, 200);
 });
 
-Route::post('/space/{building}/{id}/free', function (Request $request) {
-    
+Route::post('/space/{building}/{space}/free', function (Request $request, $building, $space) {
+    \DB::table('spaces')->insert(
+        [
+            'building' => $building, 
+            'space' => $space,
+            'free' => true,
+        ]
+    );
+    return response()->json(true, 200);
+});
+
+Route::get('/space/{building}/{space}/last_state', function (Request $request, $building, $space) {
+    $data = \DB::table('spaces')
+        ->where('building', '=', $building)
+        ->where('space', '=', $space)
+        ->orderBy('created_at', 'DESC')
+        ->first();
+    if ($data != null)
+        return response()->json(['free' => $data['free'], 'timestamp' => $data['created_at']], 200);
+    else abort(404);
 });
