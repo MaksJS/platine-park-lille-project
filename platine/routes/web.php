f4302bc8f6a133ff35516d1c6a2d2d323430ac33
Maks <maxence.derous@gmail.com>
2016-12-09 17:03:40 +0100

    added GET routes

--- platine/routes/web.php
+++ platine/routes/web.php
@@ -12,5 +12,31 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    abort(403);
+});
+
+Route::get('/spaces', function (Request $request) {
+    return response()->json(json_decode(file_get_contents('data.json'), true));
+});
+
+Route::get('/spaces/{building}', function (Request $request, $building) {
+    $data = json_decode(file_get_contents('data.json'), true);
+    if (array_key_exists($building, $data))
+        return response()->json($data[$building]);
+    else abort(404);
+});
+
+Route::get('/space/{building}/{id}', function (Request $request, $building, $id) {
+    $data = json_decode(file_get_contents('data.json'), true);    
+    if (array_key_exists($building, $data) && array_key_exists($id, $data[$building]))  
+        return response()->json($data[$building][$id]);
+    else abort(404);
+});
+
+Route::post('/space/{building}/{id}/occuped', function (Request $request) {
+    
+});
+
+Route::post('/space/{building}/{id}/free', function (Request $request) {
+    
 });
