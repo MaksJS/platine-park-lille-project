2ae68c03f953d396efa5fad31582ecc0c74d03d2
Maks <maxence.derous@gmail.com>
2016-12-14 16:46:53 +0100

    refactored web routes to API

--- platine/routes/api.php
+++ platine/routes/api.php
@@ -16,3 +16,60 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('/spaces', function (Request $request) {
+    return response()->json(json_decode(file_get_contents('data.json'), true), 200);
+})->middleware('api');
+
+Route::get('/spaces/{building}', function (Request $request, $building) {
+    $data = json_decode(file_get_contents('data.json'), true);
+    if (array_key_exists($building, $data))
+        return response()->json($data[$building], 200);
+    else abort(404);
+})->middleware('api');
+
+Route::get('/space/{building}/{space}', function (Request $request, $building, $space) {
+    $data = json_decode(file_get_contents('data.json'), true);    
+    if (array_key_exists($building, $data) && array_key_exists($space, $data[$building]))  
+        return response()->json($data[$building][$space], 200);
+    else abort(404);
+})->middleware('api');
+
+Route::post('/space/{building}/{space}/occuped', function (Request $request, $building, $space) {
+    \DB::table('spaces')->insert(
+        [
+            'building' => $building, 
+            'space' => $space,
+            'created_at' => new \DateTime(),
+            'free' => false,
+        ]
+    );
+    return response()->json(true, 200);
+})->middleware('api');
+
+Route::post('/space/{building}/{space}/free', function (Request $request, $building, $space) {
+    \DB::table('spaces')->insert(
+        [
+            'building' => $building, 
+            'space' => $space,
+            'created_at' => new \DateTime(),
+            'free' => true,
+        ]
+    );
+    return response()->json(true, 200);
+})->middleware('api');
+
+Route::get('/space/{building}/{space}/last_state', function (Request $request, $building, $space) {
+    $data = \DB::table('spaces')
+        ->where('building', '=', $building)
+        ->where('space', '=', $space)
+        ->orderBy('created_at', 'DESC')
+        ->first();
+    \Carbon\Carbon::setLocale('fr');
+    if ($data != null)
+        return response()->json([
+            'free' => boolval($data->free), 
+            'timestamp' => (new \Carbon\Carbon($data->created_at))->diffForHumans(\Carbon\Carbon::now())
+        ], 200);
+    else abort(404);
+})->middleware('api');
